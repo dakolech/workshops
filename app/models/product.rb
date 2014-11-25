1ee6f9538772825801fe79ab1ff9bb5059e6375f
Daniel Koza <dakolech@gmail.com>
2014-11-25 16:01:29 +0100

    Product model tests passes

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,17 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates :price, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }
+  validates :description, presence: true
+  validates :title, presence: true
+
+  def average_rating
+    rating = 0.0
+    reviews.each do |el|
+      rating += el.rating
+    end
+    rating /= reviews.count
+  end
+
 end
